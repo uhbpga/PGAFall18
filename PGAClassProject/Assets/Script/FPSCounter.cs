bb916211dba5f527b968960552107901d5fb6a8d
uhbpga <umair.pga@gmail.com>
2018-11-22 20:10:35 +0500

    -- First Commit For Class Project

---
+++ PGAClassProject/Assets/Script/FPSCounter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSCounter : MonoBehaviour
+{
+    private float timecheckvar;
+    private int timecountvar;
+    private int framecountvar;
+    private int totalframevar;
+    private float avgframevar;
+    
+
+    private void Awake()
+    {
+        timecountvar = 0;
+        framecountvar = 0;
+        totalframevar = 0;
+        timecheckvar = 0.0f;
+        timecheckvar = Time.realtimeSinceStartup;
+    }
+
+    private void Update()
+    {
+        framecountvar += 1;
+        
+        if (Time.realtimeSinceStartup - timecheckvar >= 1)
+        {
+            Debug.Log("Frame Count last second: " + framecountvar);
+            framecountvar = 0;
+
+            avgframevar = ((totalframevar * timecountvar) + framecountvar) / timecountvar;
+
+            timecountvar += 1;
+            Debug.Log("Average frames per second: " + avgframevar);
+            timecheckvar = 0.0f;
+        }
+    }
+
+}
---
+++ PGAClassProject/Assets/Script/PositionDetector.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionDetector : MonoBehaviour
+{
+    private bool privateVar;
+    protected bool protectedVar;
+    public bool publicVar;
+    private void Awake()
+    {
+        Debug.Log("Awake");
+    }
+
+    private void OnEnable()
+    {
+        Debug.Log("OnEnable");
+    }
+    private void Start()
+    {
+        Debug.Log("Start");
+    }
+
+    private void OnDisable()
+    {
+        Debug.Log("OnDisable");
+    }
+
+    private void Update()
+    {
+        
+    }
+    private void LateUpdate()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    private void OnGUI()
+    {
+        if (GUI.Button(new Rect(Screen.width*0.25f,Screen.height*0.1f,Screen.width*0.2f,Screen.height*0.1f),"Button"))
+        {
+
+        }
+    }
+}
