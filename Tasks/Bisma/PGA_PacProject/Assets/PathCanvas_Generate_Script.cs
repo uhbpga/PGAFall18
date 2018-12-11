a16e030a368f59594bf928f413601b4a883a1d7f
doldydo <bisma.ziaa@gmail.com>
2018-12-11 17:12:21 +0500

    Basic Canvas for path generation

---
+++ Tasks/Bisma/PGA_PacProject/Assets/PathCanvas_Generate_Script.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PathCanvas_Generate_Script : MonoBehaviour {
+
+    DynamicPathGeneration pathGenScript;
+    Button genbutton;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void pooling()
+    {
+        pathGenScript.Pooling();
+
+    }
+    public void makePath()
+    {
+        pathGenScript.GenerateNewPath();
+    }
+
+}
