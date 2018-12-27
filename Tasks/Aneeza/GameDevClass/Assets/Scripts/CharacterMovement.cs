ba5d71fba9a5c61d2c3b2073273063dc5783967b
Aneeza Ashraf <aashr001@gold.ac.uk>
2018-12-27 15:13:02 +0500

    GameDev

---
+++ Tasks/Aneeza/GameDevClass/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour {
+
+    public CharacterController characterController_Player;
+    private float gravity = 2;
+    private float jump = 15;
+    private Vector3 moveDirection;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        moveDirection = Vector3.zero;
+
+        //for keyboard
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            moveDirection += characterController_Player.transform.forward;
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            moveDirection -= characterController_Player.transform.forward;
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            moveDirection += characterController_Player.transform.right;
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            moveDirection -= characterController_Player.transform.right;
+        }
+
+        //for joystick
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        moveDirection *= Time.deltaTime;
+
+        //for jump
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            moveDirection += Vector3.up * jump;
+            if (characterController_Player.isGrounded) //so it can't jump in the air
+            {
+               
+            }
+        }
+
+       moveDirection = characterController_Player.transform.forward;
+       moveDirection += Vector3.down * gravity * Time.deltaTime;
+       characterController_Player.Move(moveDirection);
+
+		
+	}
+}
