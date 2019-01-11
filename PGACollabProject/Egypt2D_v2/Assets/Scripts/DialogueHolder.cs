7cf42babb14a474220649c532a96732030134206
Aneeza Ashraf <aashr001@gold.ac.uk>
2019-01-11 13:44:05 +0500

    Dialogue System implemented.

---
+++ PGACollabProject/Egypt2D_v2/Assets/Scripts/DialogueHolder.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DialogueHolder : MonoBehaviour
+{
+
+    public string dialogue;
+    private DialogueManager dManager;
+
+    // Use this for initialization
+    void Start()
+    {
+        dManager = FindObjectOfType<DialogueManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Trigger zone entered");
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("Trigger zone exit");
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                dManager.ShowBox(dialogue);
+            }
+            
+        }
+    }
+}
